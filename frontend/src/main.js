90e5670c592d7a2b714aad9baadd95c06eaf1baa
Alex Chan <alexchanwk@gmail.com>
2024-02-23 16:11:18 +0800

    hide login form for non-interactive authentication

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,36 +16,46 @@ sync(store, router);
 async function start() {
   try {
     if (loginPage) {
-      await validateLogin();
+      try {
+        await validateLogin();
+      } catch (e) {
+        console.log(e);
+      }
+
+      if (recaptcha) {
+        await new Promise((resolve) => {
+          const check = () => {
+            if (typeof window.grecaptcha === "undefined") {
+              setTimeout(check, 100);
+            } else {
+              resolve();
+            }
+          };
+    
+          check();
+        });
+      }
+    
+      new Vue({
+        el: "#app",
+        store,
+        router,
+        i18n,
+        template: "<App/>",
+        components: { App },
+      });
+
     } else {
       await login("", "", "");
     }
   } catch (e) {
-    console.log(e);
+    new Vue({ el: "#app", template: null });
+
+    setTimeout(function () {
+      const loading = document.getElementById("loading");
+      loading.parentNode.removeChild(loading);
+    }, 1000);
   }
-
-  if (recaptcha) {
-    await new Promise((resolve) => {
-      const check = () => {
-        if (typeof window.grecaptcha === "undefined") {
-          setTimeout(check, 100);
-        } else {
-          resolve();
-        }
-      };
-
-      check();
-    });
-  }
-
-  new Vue({
-    el: "#app",
-    store,
-    router,
-    i18n,
-    template: "<App/>",
-    components: { App },
-  });
 }
 
 start();
